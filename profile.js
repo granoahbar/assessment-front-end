9ae12ad13071ab7ddff685fb38060f01d710c3dc
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-02 12:08:28 -0600

    part 5 done

---
+++ profile.js
@@ -0,0 +1,21 @@
+let colorBtn = document.querySelector('#color')
+
+let placeBtn = document.querySelector('#place')
+
+let ritualBtn = document.querySelector('#ritual')
+
+
+
+
+colorBtn.addEventListener('click', () => {
+    alert('Green and blue are my favorite colors!');
+})
+
+placeBtn.addEventListener('click', () => {
+    alert('Oregon is my favorite place!');
+})
+
+ritualBtn.addEventListener('click', () => {
+    alert('I love to lift weights every day! It helps me feel happier and healthier all around.');
+})
+
