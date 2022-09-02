062075d23acec8d1bed1cd5956ee12b004aa5722
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-02 10:32:55 -0600

    part 2 done

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form has been submitted successfully!');
+}
+
+function catPhotoComp(evt) {
+	evt.preventDefault();
+
+	alert('Hey Sexy!')
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let img = document.querySelector('#catphoto')
+
+img.addEventListener('mouseover', catPhotoComp)
